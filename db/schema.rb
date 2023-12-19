a90248b12faaf6c3aba059c438fca7e5cf469ad3
shivkanya patel <shiv.yuvasoft@gmail.com>
2023-12-19 13:00:15 +0530

    order skeleton added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_19_072247) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_19_072936) do
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "full_name"
     t.string "email"
@@ -19,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_19_072247) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "orders", "users"
 end
